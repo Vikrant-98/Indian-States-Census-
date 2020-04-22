acc76d6721b2493ce7a9f6a236f100cd710ec9eb
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-23 01:39:46 +0530

    Invalid Type check pass

--- NUnitTestCensus/UnitTest1.cs
+++ NUnitTestCensus/UnitTest1.cs
@@ -22,5 +22,11 @@ namespace Tests
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("There is no Such Files", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt"));
         }
+        [Test]
+        public void Test3()
+        {
+            StateCensusAnalyser matches = new StateCensusAnalyser();
+            Assert.AreEqual("File Contains invalid records", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
+        }
     }
 }
