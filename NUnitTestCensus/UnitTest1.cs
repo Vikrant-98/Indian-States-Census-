8a158d4db6171eea5791f0aaf3e0fe60187af7b2
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-23 14:03:44 +0530

    Record matching check pass

--- NUnitTestCensus/UnitTest1.cs
+++ NUnitTestCensus/UnitTest1.cs
@@ -11,35 +11,40 @@ namespace Tests
         }
 
         [Test]
-        public void Testcase1()
+        public void Testcase1_1()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("HAPPY",matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
         [Test]
-        public void Testcase2()
+        public void Testcase1_2()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("There is no Such Files", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt"));
         }
         [Test]
-        public void Testcase3()
+        public void Testcase1_3()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("File Contains invalid records", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
         [Test]
-        public void Testcase4()
+        public void Testcase1_4()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("There is no Such Files", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt"));
         }
         [Test]
-        public void Testcase5()
+        public void Testcase1_5()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("File contains invalid Headers", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
-
+        [Test]
+        public void Testcase2_1()
+        {
+            CSVStateCensus matches = new CSVStateCensus();
+            Assert.AreEqual("HAPPY", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv"));
+        }
     }
 }
