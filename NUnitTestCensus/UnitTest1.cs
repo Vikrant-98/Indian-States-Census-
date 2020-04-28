68526c20a335c03eab62bb69ecbd7a831632e2d9
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-28 12:39:35 +0530

    Map the State census data

--- NUnitTestCensus/UnitTest1.cs
+++ NUnitTestCensus/UnitTest1.cs
@@ -5,95 +5,80 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Testcase1_1()
+        public void GiventheStatesWhenAnalyseShouldRecordNumberOfRecordmatches()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("HAPPY", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
         [Test]
-        public void Testcase1_2()
+        public void GiventheStateCensusCSVFileincorrectReturnscustomException()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("There is No Such Files", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt"));
         }
         [Test]
-        public void Testcase1_3()
+        public void GiventheStateCensusCSV_FileWhenCorrectbuttypeincorrectReturnscustomException()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("File Contains Invalid Records", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
+        
         [Test]
-        public void Testcase1_4()
-        {
-            StateCensusAnalyser matches = new StateCensusAnalyser();
-            Assert.AreEqual("There is No Such Files", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt"));
-        }
-        [Test]
-        public void Testcase1_5()
+        public void GiventheStateCensusCSV_FilewhenCorrectbutcsvheaderincorrectReturnsCustomException()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             Assert.AreEqual("File Contains Invalid Headers", matches.StateCensusAnalyzer(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv"));
         }
         [Test]
-        public void Testcase2_1()
+        public void GiventheStatesCodeWhenAnalyseShouldRecordNumberOfRecordmatches()
         {
             CSVStateCensus matches = new CSVStateCensus();
             Assert.AreEqual("HAPPY", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv"));
         }
         [Test]
-        public void Testcase2_2()
+        public void Given_the_StateCode_CSV_File_incorrect_Returns_custom_Exception()
         {
             CSVStateCensus matches = new CSVStateCensus();
             Assert.AreEqual("There is No Such Files", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.txt"));
         }
         [Test]
-        public void Testcase2_3()
+        public void GiventheStateCodeCSVFile_correct_but_type_incorrect_Returns_custom_Exception()
         {
             CSVStateCensus matches = new CSVStateCensus();
             Assert.AreEqual("File Contains Invalid Records", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv"));
         }
+        
         [Test]
-        public void Testcase2_4()
-        {
-            CSVStateCensus matches = new CSVStateCensus();
-            Assert.AreEqual("There is No Such Files", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.txt"));
-        }
-        [Test]
-        public void Testcase2_5()
+        public void GiventheStateCodeCSVFile_correctButcsv_header_incorrect_Returns_custom_Exception()
         {
 
             CSVStateCensus matches = new CSVStateCensus();
             Assert.AreEqual("File Contains Invalid Headers", matches.CSVStatesCensus(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv"));
         }
         [Test]
-        public void Testcase3()
+        public void Statealphabeticalorder_First()
         {
             Factory factory = new Factory();
             string actual = factory.StateEntry(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv",0, 1);
             Assert.AreEqual("Arunachal Pradesh", actual);
         }
         [Test]
-        public void Testcase3_1()
+        public void Statealphabeticalorder_Last()
         {
             Factory factory = new Factory();
             string actual = factory.StateEntry(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv", 0, 29);
             Assert.AreEqual("West Bengal", actual);
         }
         [Test]
-        public void Testcase4()
+        public void StateCodeinanalphabeticalorder_First()
         {
             Factory factory = new Factory();
             string actual = factory.StateCodeEntry(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv", 0, 1);
             Assert.AreEqual("Andhra Pradesh", actual);
         }
         [Test]
-        public void Testcase4_1()
+        public void StateCodeinanalphabeticalorder_Last()
         {
             Factory factory = new Factory();
             string actual = factory.StateCodeEntry(@"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv", 0, 37);
