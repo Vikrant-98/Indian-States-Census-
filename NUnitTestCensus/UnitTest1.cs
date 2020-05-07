6faba0558e4065aa0424bbdc054a029e165b80f1
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-07 13:29:17 +0530

    Test Cases Added for US Census Data

--- NUnitTestCensus/UnitTest1.cs
+++ NUnitTestCensus/UnitTest1.cs
@@ -1,10 +1,17 @@
 using NUnit.Framework;
 using IndianStatesCensus;
+using static IndianStatesCensus.StateCodeDAO;
+using static IndianStatesCensus.US_CensusDataDAO;
+using static IndianStatesCensus.StateCensusAnalyser;
+
 
 namespace Tests
 {
     public class Tests
     {
+        readonly GetCSVCount csvstatecensus = Factory.DelegateofStateCensusAnalyse();
+        readonly GetCountFromCSVStates statesCodeCSV = Factory.DelegateofStatecode();
+        readonly GetUSCSVCount statecens_US_CSV = Factory.DelegateofUSCensusData();
         /// <summary>
         /// TC-1.1: Test for checking number of Records
         /// </summary>
@@ -13,7 +20,7 @@ namespace Tests
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv";
-            Assert.AreEqual("", matches.StateCensusAnalyzer(filepath));
+            Assert.AreEqual("30", matches.StateCensusAnalyzer(filepath));
         }
         /// <summary>
         /// TC-1.2:If file incorrect then throw custom exception
@@ -22,7 +29,7 @@ namespace Tests
         public void GiventheStateCensusCSVFileincorrectReturnscustomException()
         {
             StateCensusAnalyser matches = new StateCensusAnalyser();
-            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt" ;
+            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.txt";
             Assert.AreEqual("There is No Such Files", matches.StateCensusAnalyzer(filepath));
         }
         /// <summary>
@@ -51,9 +58,9 @@ namespace Tests
         [Test]
         public void GiventheStatesCodeWhenAnalyseShouldRecordNumberOfRecordmatches()
         {
-            CSVStateCensus matches = new CSVStateCensus();
+            StateCodeDAO matches = new StateCodeDAO();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv";
-            Assert.AreEqual("", matches.CSVStatesCensus(filepath));
+            Assert.AreEqual("38", matches.CSVStatesCode(filepath));
         }
         /// <summary>
         /// TC-2.2:If file incorrect then throw custom exception
@@ -61,9 +68,9 @@ namespace Tests
         [Test]
         public void Given_the_StateCode_CSV_File_incorrect_Returns_custom_Exception()
         {
-            CSVStateCensus matches = new CSVStateCensus();
+            StateCodeDAO matches = new StateCodeDAO();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.txt";
-            Assert.AreEqual("There is No Such Files", matches.CSVStatesCensus(filepath));
+            Assert.AreEqual("There is No Such Files", matches.CSVStatesCode(filepath));
         }
         /// <summary>
         /// TC-2.3:If Record incorrect then throw custom exception
@@ -71,9 +78,9 @@ namespace Tests
         [Test]
         public void GiventheStateCodeCSVFile_correct_but_type_incorrect_Returns_custom_Exception()
         {
-            CSVStateCensus matches = new CSVStateCensus();
+            StateCodeDAO matches = new StateCodeDAO();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv";
-            Assert.AreEqual("File Contains Invalid Records", matches.CSVStatesCensus(filepath));
+            Assert.AreEqual("File Contains Invalid Records", matches.CSVStatesCode(filepath));
         }
         /// <summary>
         /// TC-2.4:If Header incorrect then throw custom exception
@@ -81,53 +88,50 @@ namespace Tests
         [Test]
         public void GiventheStateCodeCSVFile_correctButcsv_header_incorrect_Returns_custom_Exception()
         {
-
-            CSVStateCensus matches = new CSVStateCensus();
+            StateCodeDAO matches = new StateCodeDAO();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv";
-            Assert.AreEqual("File Contains Invalid Headers", matches.CSVStatesCensus(filepath));
+            Assert.AreEqual("File Contains Invalid Headers", matches.CSVStatesCode(filepath));
         }
+
         /// <summary>
         /// Arrange stateSencus data in Alphabetical order first state
         /// </summary>
         [Test]
-        public void State_Alphabeticalorder_First()
+        public void Indian_State_Alphabeticalorder_First()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv";
-            string actual = factory.StateEntry(filepath,0, 1);
+            string actual = csvstatecensus(filepath, 0, 1);
             Assert.AreEqual("Andhra Pradesh", actual);
         }
         /// <summary>
         /// Arrange StateSencus data in Alphabetical order last state
         /// </summary>
         [Test]
-        public void State_Alphabeticalorder_Last()
+        public void Indian_State_Alphabeticalorder_Last()
         {
-            Factory factory = new Factory();
+            
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv";
-            string actual = factory.StateEntry(filepath, 0, 29);
+            string actual = csvstatecensus(filepath, 0, 29);
             Assert.AreEqual("West Bengal", actual);
         }
         /// <summary>
         /// Arrange StateCode data in Alphabetical order First state
         /// </summary>
         [Test]
-        public void StateCodeinanalphabeticalorder_First()
+        public void Indian_StateCode_In_An_AlphbeticalOrder_First()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv";
-            string actual = factory.StateCodeEntry(filepath, 0, 1);
+            string actual = statesCodeCSV(filepath, 0, 1);
             Assert.AreEqual("Andaman and Nicobar Islands", actual);
         }
         /// <summary>
         /// Arrange StateCode data in Alphabetical order Last state
         /// </summary>
         [Test]
-        public void StateCodeiInAnalphAbeticalOrder_Last()
+        public void Indian_StateCode_In_An_AlphbeticalOrder_Last()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCode.csv";
-            string actual = factory.StateCodeEntry(filepath, 0, 37);
+            string actual = statesCodeCSV(filepath, 0, 37);
             Assert.AreEqual("West Bengal", actual);
         }
         /// <summary>
@@ -136,10 +140,49 @@ namespace Tests
         [Test]
         public void PassReportNumberOfStatesSorted()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\StateCensusData.csv";
-            string actual = factory.StateEntry(filepath, 0, 37);
+            string actual = csvstatecensus(filepath, 0, 37);
             Assert.AreEqual("0", actual);
+        }
+        /// <summary>
+        /// TC-3.1: Test for checking number of Records
+        /// </summary>
+        [Test]
+        public void Giventhe_US_StatesCodeWhenAnalyseShouldRecordNumberOfRecordmatches()
+        {
+            US_CensusDataDAO US_states = new US_CensusDataDAO();
+            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
+            Assert.AreEqual("52", US_states.StateCensusAnalyzer(filepath));
+        }
+        /// <summary>
+        /// TC-3.2:If file incorrect then throw custom exception
+        /// </summary>
+        [Test]
+        public void Giventhe_US_StateCensusCSVFileincorrectReturnscustomException()
+        {
+            US_CensusDataDAO US_states = new US_CensusDataDAO();
+            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.txt";
+            Assert.AreEqual("There is No Such Files", US_states.StateCensusAnalyzer(filepath));
+        }
+        /// <summary>
+        /// TC-3.3:If Record incorrect then throw custom exception
+        /// </summary>
+        [Test]
+        public void Giventhe_US_StateCensusCSV_File_type_IncorrectReturn_Exception()
+        {
+            US_CensusDataDAO US_states = new US_CensusDataDAO();
+            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
+            Assert.AreEqual("File Contains Invalid Records", US_states.StateCensusAnalyzer(filepath));
+        }
+        /// <summary>
+        /// TC-3.4:If Header incorrect then throw custom exception
+        /// </summary>
+        [Test]
+        public void Giventhe_US_StateCensusCSV_FilewhenCorrectbutcsvheaderincorrectReturnsCustomException()
+        {
+            US_CensusDataDAO US_states = new US_CensusDataDAO();
+            string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
+            Assert.AreEqual("File Contains Invalid Headers", US_states.StateCensusAnalyzer(filepath));
         }
         /// <summary>
         /// US data of highest population density State
@@ -147,9 +190,8 @@ namespace Tests
         [Test]
         public void Given_Population_Density_Most_US_Populated_States()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
-            string actual = factory.US_CensusData(filepath, 0, 1);
+            string actual = statecens_US_CSV(filepath, 0, 1);
             Assert.AreEqual("District of Columbia", actual);
         }
         /// <summary>
@@ -158,9 +200,8 @@ namespace Tests
         [Test]
         public void US_State_in_analphabetical_Order_First()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
-            string actual = factory.US_CensusData(filepath, 0, 1);
+            string actual = statecens_US_CSV(filepath, 0, 1);
             Assert.AreEqual("Alabama", actual);
         }
         /// <summary>
@@ -169,9 +210,8 @@ namespace Tests
         [Test]
         public void US_State_in_analphabetical_Order_Last()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
-            string actual = factory.US_CensusData(filepath, 0, 51);
+            string actual = statecens_US_CSV(filepath, 0, 51);
             Assert.AreEqual("Wyoming", actual);
         }
         /// <summary>
@@ -180,9 +220,8 @@ namespace Tests
         [Test]
         public void US_State_in_TotalArea_Order_First_States()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
-            string actual = factory.US_CensusData(filepath, 0, 1);
+            string actual = statecens_US_CSV(filepath, 0, 1);
             Assert.AreEqual("Alaska", actual);
         }
         /// <summary>
@@ -191,9 +230,8 @@ namespace Tests
         [Test]
         public void PassReportNumberOf_US_StatesSorted()
         {
-            Factory factory = new Factory();
             string filepath = @"C:\Users\The Daddy\source\repos\IndianStatesCensus\IndianStatesCensus\USCensusData.csv";
-            string actual = factory.US_CensusData(filepath, 0, 37);
+            string actual = statecens_US_CSV(filepath, 0, 37);
             Assert.AreEqual("623", actual);
         }
     }
